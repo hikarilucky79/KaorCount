2c0395b67c1987e745753bb849d54b6791ac8e58
Eduardo-Luiz-Campos <campos.santos.eduardo@escola.pr.gov.br>
2026-07-31 12:21:07 -0300

    Usuarios e Meta foram fundidos no banco.sql e correção de erros

---
+++ backend/banco_de_dados/banco.sql
@@ -0,0 +1,41 @@
+/*Cria e usa a database*/
+
+CREATE DATABASE IF NOT EXISTS KaourCount;
+USE KaourCount;
+
+  /* CRIA AS TABELAS */
+
+CREATE TABLE IF NOT EXISTS USUARIOS (
+  id_user INT PRIMARY KEY AUTO_INCREMENT,
+
+  nome VARCHAR(225) NOT NULL,
+  sobrenome VARCHAR(225) NOT NULL,
+  altura DECIMAL(3,2) NOT NULL, 
+  data_nascimento DATE NOT NULL, 
+  sexo CHAR NOT NULL,
+  objetivo CHAR DEFAULT('P'),
+  nivel_atv CHAR NOT NULL,
+  email VARCHAR(225) NOT NULL UNIQUE,
+  senha VARCHAR(225) NOT NULL, 
+  data_cadastro DATE NOT NULL,
+  status_conta CHAR DEFAULT('D') ,
+
+  CONSTRAINT chk_email CHECK (email LIKE '%@%'),
+  CONSTRAINT chk_sexo CHECK (sexo IN ('F', 'M')), /*feminino, masculino*/
+  CONSTRAINT chk_objetivo CHECK (objetivo IN ('P', 'M', 'G')), /*perder,manter, ganhar*/
+  CONSTRAINT chk_nivel_atv CHECK (nivel_atv IN ('S', 'L', 'N', 'M')) /*sedentario, levemente_atv, moderadamente_atv, muito_atv*/
+);
+
+CREATE TABLE IF NOT EXISTS META_NUTRI (
+  id_meta INT PRIMARY KEY AUTO_INCREMENT,
+
+  caloria_diaria INT NOT NULL,
+  carboidrato_g DECIMAL(5,2),
+  proteina_g DECIMAL(5,2) ,
+  gordura_g DECIMAL(5,2),
+  data_inicio DATE NOT NULL,
+
+  id_user INT NOT NULL,
+
+  FOREIGN KEY(id_user) REFERENCES USUARIOS
+);
